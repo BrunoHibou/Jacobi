58d3e212c1499d19ec888a6297652083d2093353
BrunoHibou <brunocavalcanticosta.bc@gmail.com>
2023-06-11 23:48:49 +0000

    new?

--- main.py
+++ main.py
@@ -29,17 +29,18 @@ def jacobi(A, b, x0, max_iterations=100, tolerance=1e-6):
 #Funçao para captura de input para solução
 def get_matrix_from_input(n):
     matrix = np.zeros((n, n))
-    print("Enter the elements of the matrix:")
+    print("Insira os elementos da matriz: ")
 
     for i in range(n):
         for j in range(n):
-            matrix[i][j] = float(input(f"Enter element at position ({i+1},{j+1}): "))
+            matrix[i][j] = float(input(f"Insira valor na posiçao({i+1},{j+1}): "))
 
     return matrix
 
 
 
 int_n = int(input("insira o grau da matriz: "))
+print(int_n)
 
 '''
 A = np.array([[4, 1, -1],
@@ -47,11 +48,17 @@ A = np.array([[4, 1, -1],
               [2, -1, 3]])
 '''
 A = get_matrix_from_input(int_n)
+print(A)
 
-b = np.array([5, 7, 3])
+#b = np.array([5, 7, 3])
 
+b = np.zeros(int_n, dtype=int)
+for p in range(int_n):
+    b[p] = int(input(f"digite um valor para b na posiçao {p}: "))
+
+print(b)
 x0 = np.zeros_like(b)
 
-solution = jacobi(A, b, x0,)
+solution = jacobi(A, b, x0)
 
 print("Solution:", solution)
